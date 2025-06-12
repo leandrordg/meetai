e3839422ae953e2f2633656e5534dcdd5f736397
Leandro Rodrigues <lj251004@gmail.com>
2025-06-11 22:06:53 -0300

    feat: add meetings functionality with CRUD operations and UI components; implement meeting status and enhance sidebar navigation

--- src/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ src/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -60,26 +60,29 @@ export function DashboardSidebar() {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              {firstSection.map((item) => (
-                <SidebarMenuItem key={item.href}>
-                  <SidebarMenuButton
-                    className={cn(
-                      "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
-                      pathname === item.href &&
-                        "bg-linear-to-r/oklch border-[#5D6B68]/10"
-                    )}
-                    isActive={pathname === item.href}
-                    asChild
-                  >
-                    <Link href={item.href}>
-                      <item.icon />
-                      <span className="text-sm font-medium tracking-tight">
-                        {item.label}
-                      </span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
+              {firstSection.map((item) => {
+                const isActive = pathname.startsWith(item.href);
+
+                return (
+                  <SidebarMenuItem key={item.href}>
+                    <SidebarMenuButton
+                      isActive={isActive}
+                      className={cn(
+                        "h-10 hover:bg-linear-to-r/oklch border border-transparent hover:border-[#5D6B68]/10 from-sidebar-accent from-5% via-30% via-sidebar/50 to-sidebar/50",
+                        isActive && "bg-linear-to-r/oklch border-[#5D6B68]/10"
+                      )}
+                      asChild
+                    >
+                      <Link href={item.href}>
+                        <item.icon />
+                        <span className="text-sm font-medium tracking-tight">
+                          {item.label}
+                        </span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                );
+              })}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
